ceb504caba5ea0315cadf12f98da799a267c862d
ThePlayerRolo <thesolis78243@gmail.com>
2025-06-06 10:05:46 -0400

    Two Things

--- src/8A420.c
+++ src/8A420.c
@@ -1,3 +1,17 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/8A420", func_80089820_8A420);
+s32 func_80089820_8A420(f32 arg0, f32 arg1) {
+    s32 var_v0;
+    s32 var_v1;
+
+    var_v0 = 1;
+    if (!(-arg1 <= arg0)) {
+        var_v0 = 0;
+    }
+    var_v1 = 1;
+    if (!(arg0 <= arg1)) {
+        var_v1 = 0;
+    }
+    return var_v0 & var_v1;
+}
+
--- src/8A460.c
+++ src/8A460.c
@@ -1,3 +1,15 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/8A460", func_80089860_8A460);
+extern f32 D_800A12A4_A1EA4;
+
+
+s32 func_80089860_8A460(f32 arg0) {
+    s32 var_v0;
+
+    var_v0 = 0;
+    if ((-D_800A12A4_A1EA4 <= arg0) && (arg0 <= D_800A12A4_A1EA4)) {
+        var_v0 = 1;
+    }
+    return var_v0;
+}
+
